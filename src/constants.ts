edd545b0548f5c152074c9616fb13154b33c50e4
winton <mail@wintoni.us>
2019-10-23 01:06:19 -0400

    Fix REGEX_FN for browser

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ012
 )
 export const ARROW = " < "
 export const REGEX_COMMENT = /\/\*[\s\S]*?\*\/|([^:]|^)\/\/.*$/gm
-export const REGEX_FN = /^(\(|function \w*\()?\s*(lid_?)[\),\s]/
+export const REGEX_FN = /^(\(|function\s*\w*\()?\s*(lid_?)[\),\s]/
 export const REGEX_ID = /(\*{1,2})|([^\.]+)\.(.+)/i
