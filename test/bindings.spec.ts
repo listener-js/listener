81d25674375e5fb17655b7b12b9549e2ddf96162
winton <mail@wintoni.us>
2019-10-23 22:23:34 -0400

    Streamline builds with typescript project references

--- test/bindings.spec.ts
+++ test/bindings.spec.ts
@@ -1,5 +1,5 @@
 import expect from "expect"
-import { Bindings } from "../dist/bindings"
+import { Bindings } from "../src/bindings"
 
 it("bindings (basic)", (): void => {
   const { bindings } = new Bindings().add("test")
